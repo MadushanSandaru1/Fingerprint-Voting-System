2e01c744d382d14c93f4cea1405fdcbe9717a37e
SampathSandaru <54311694+SampathSandaru@users.noreply.github.com>
2020-01-03 23:30:39 +0530

    little edited

--- html/admin_addAssistantElectionOfficer.php
+++ html/admin_addAssistantElectionOfficer.php
@@ -128,7 +128,7 @@
         <!-- google font -->
         <link href='https://fonts.googleapis.com/css?family=Baloo Chettan' rel='stylesheet'>
         
-        <!-- email search-->
+        <!-- dropdown search-->
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
